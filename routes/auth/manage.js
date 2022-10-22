b72123d1febb722314aa338e6bdbdf7d145a0b5e
CoderParth <ghantigalli@gmail.com>
2022-10-23 01:56:33 +1100

    Changes in manage.js

--- routes/auth/manage.js
+++ routes/auth/manage.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../../controllers/authController');
 const User = require("../../models/userModel");
 const Staff = require("../../models/staffModel");
-const Timesheet = require("../../models/timesheetModel");
+
 
 router.get('/', auth, async (request, response,) => {
 	const staffs = await Staff.find({ user: request.user.userId });
