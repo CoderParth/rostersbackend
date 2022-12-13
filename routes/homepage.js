e88e6966a1b22a390362f8672e14d8f7ecba10b5
CoderParth <82562466+CoderParth@users.noreply.github.com>
2022-12-13 18:14:22 +1100

    Update homepage.js

--- routes/homepage.js
+++ routes/homepage.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (request, response) => {
-	response.send('<html> A COMPLETE BACKEND MADE FOR "roster management like app"\nCheck out the source code and doumentation on <a href="https://github.com/CoderParth/rostersbackend"> github</a> </html>');
+	response.send('<html> A REST API built FOR "roster management like app"\nCheck out the source code and doumentation on <a href="https://github.com/CoderParth/rostersbackend"> github</a> </html>');
 });
 module.exports = router;
